4d4530f1831afd1f7f0fc96b2c8a92606eafe1ac
jagajastic <josephibrahi@gmail.com>
2020-01-10 09:58:41 +0100

    add enhance loop

---
+++ Array/enhanceforloop.java
@@ -0,0 +1,15 @@
+package Array;
+
+/**
+ * enhanceforloop
+ */
+public class enhanceforloop {
+
+    public static void main(String[] args) {
+        int[] newArr = { 1, 2, 3, 4, 5, 6, 7 };
+
+        for (int item : newArr) {
+            System.out.println(item);
+        }
+    }
+}
