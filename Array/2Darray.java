5907aa0e9e7bdcb5e1d1cfb34b5e87774848fe1d
jagajastic <josephibrahi@gmail.com>
2020-01-10 09:56:49 +0100

    ad two dimensional array and looping over the 2D Array

---
+++ Array/2Darray.java
@@ -0,0 +1,12 @@
+
+class TwoDimensionalArray {
+    public static void main(String[] args) {
+        // two dimensional array
+
+        int[][] twoDArray = { { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, };
+
+        for (int i = 0; i < twoDArray.length; i++) {
+            System.out.println(java.util.Arrays.toString(twoDArray[i]));
+        }
+    }
+}
